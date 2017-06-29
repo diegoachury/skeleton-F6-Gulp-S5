b12c1917ad174c49b8952545ba6eed51b00cbdb1
Diego MSI <diego_msi@gmail.com>
2017-06-29 17:19:10 -0500

    ajuste default de skeleton

---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+global.$       = global.jQuery = require('jquery');
+var foundation = require('../../node_modules/foundation-sites/dist/js/foundation.js');
+var moment = require('moment');
+var ale = require('./alert');
+$(document).foundation();
